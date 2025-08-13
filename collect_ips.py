b38333a9f3fb123a518e629fabd9f7d82f4357d2
yfl888 <50236927+yfl888@users.noreply.github.com>
2025-08-13 20:18:58 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -15,8 +15,8 @@ ipv4_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
 ipv6_pattern = r'\b(?:[A-Fa-f0-9]{0,4}:){2,7}[A-Fa-f0-9]{0,4}\b'
 
 # 检查ipv4/ipv6.txt文件是否存在,如果存在则删除它
-if os.path.exists('ipv4.txt','ipv6.txt'):
-    os.remove('ipv4.txt','ipv6.txt')
+if os.path.exists('ipv4.txt, ipv6.txt'):
+    os.remove('ipv4.txt, ipv6.txt')
 
 print("[INFO] 开始抓取 Cloudflare IP ...")
 
